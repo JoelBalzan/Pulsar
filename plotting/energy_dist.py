fb682b3c5e5b0bd9a0658eb35f0c453aebbd9fe4
JoelBalzan <19011859@student.westernsydney.edu.au>
2022-11-28 11:48:17 +1100

    update

--- plotting/energy_dist.py
+++ plotting/energy_dist.py
@@ -7,6 +7,9 @@ import os
 import pandas as pd
 from scipy.signal import find_peaks, peak_widths
 
+# python energy_dist.py period
+
+
 # top 10 fluxes
 fluxes = np.empty(0)
 # indexes of top 10 fluxes
@@ -14,31 +17,38 @@ fluxes_i = np.empty(0)
 #widths in ms
 widths = np.empty(0)
 #spin period of object
-period = 1/float(sys.argv[3])
+period = 1/float(sys.argv[1])
 
 
 for ar in glob.glob("*.rescaled"):
     #a = psrchive.Archive_load(sys.argv[1])
     a = psrchive.Archive_load(ar)
-    a.remove_baseline()
-    a.tscrunch()
-    a.fscrunch()
-    a.pscrunch()
-    data = a.get_data()
+    c = a.clone()
+    c.remove_baseline()
+    c.tscrunch()
+    c.fscrunch()
+    c.pscrunch()
+    data = c.get_data()
     nsub, npol, nchan, nbin = data.shape
 
-    p1 = float(sys.argv[1])
-    p2 = float(sys.argv[2])
+    
+    # peak and index
+    flux = data[0,0,0,:]
+    peaks, _ = find_peaks(flux)
+    peak_flux = np.sort(flux[peaks])[-1]
+    peak_idx = np.where(flux==peak_flux)[0][0]
+
+    # on-pulse phase start and finish
+    p1 = np.round(peak_idx/nbin - 0.05, 4)
+    p2 = np.round(peak_idx/nbin + 0.05, 4)
     ps = int(p1*nbin)
     pf = int(p2*nbin)
-
 
     flux = data[0,0,0,ps:pf]
     peaks, _ = find_peaks(flux)
     # highest 10 fluxes
     flux10 = np.sort(flux[peaks])[::-1][0:10]
     fluxes = np.concatenate((fluxes,flux10), axis=None)
-
 
     # index of highest 10 fluxes
     idxs = []
@@ -63,11 +73,13 @@ fluence = fluxes*widths
 plt.figure(figsize=(15,15),dpi=300)
 ax = plt.subplot(111)
 plt.hist(fluence, bins=50, histtype='step')
-plt.xlabel('Fluence (Jy ms)')
-plt.ylabel('Count')
+ax.set_xscale("log")
+ax.set_yscale("log")
+plt.xlabel('log(Fluence) (Jy ms)')
+plt.ylabel('log(Count)')
 plt.title('P970')
 
-plt.savefig("e_dist_P970.png")
+plt.savefig("e_dist_PX500_38329.png")
 
 
 
