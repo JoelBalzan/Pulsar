8e15ed7b7c2a44af8e1d50dc95b94239ca7381d5
JoelBalzan <jbalzan.astro@gmail.com>
2023-05-06 16:48:43 +1000

    updates

--- plotting/GMPs.py
+++ plotting/GMPs.py
@@ -3,12 +3,15 @@ import os
 import numpy as np
 import glob
 import matplotlib.pyplot as plt
+import sys
+import matplotlib.cm as cm
 
 #########################
 ### GIANT MICROPULSES ###
 #########################
 
-PCODE = "P970"
+#project code
+PCODE = sys.argv[1]
 files = sorted(glob.glob("*.rescaled"))
 if os.path.isfile('pulse_profiles_'+PCODE+'.npy'):
     pulse_profiles = np.load('pulse_profiles_'+PCODE+'.npy')
@@ -33,20 +36,25 @@ nfile, nbin = np.shape(pulse_profiles)
 
 mean_profile = np.mean(pulse_profiles, axis=0)
 
-fig = plt.figure(figsize=(10,5), dpi=300)
 for i in range(nfile):
+    fig = plt.figure(figsize=(10,5), dpi=300)
     gmps = []
+    gmps_i = []
     for j in range(nbin):
-        if pulse_profiles[i][j]/mean_profile[j] > 1000:
-            gmps.append(j)
-    plt.plot(pulse_profiles[i], c='k', lw=0.5, label='Pulse Profile')
-    plt.plot(pulse_profiles[i]/mean_profile, c='b', lw=0.5, linestyle='--', label='Pulse Profile / Mean Profile')
-    plt.xlabel('Phase Bin')
-    plt.ylabel('Flux Density (Jy)')
-    for k in range(len(gmps)):
-        plt.axvline(x=gmps[k], c='r', lw=0.5, linestyle='--')
-    plt.legend()
-    plt.savefig('GMP_'+'_'+files[i].split(os.extsep, 1)[0]+'.pdf', dpi=300)
+        if pulse_profiles[i][j]/mean_profile[j] > 100:
+            gmps.append(pulse_profiles[i][j]/mean_profile[j])
+            gmps_i.append(j)
+    if len(gmps) != 0:
+        colours = cm.tab20(np.linspace(0, 1, len(gmps)))
+        plt.plot(pulse_profiles[i], c='k', lw=0.5, label='Pulse Profile')
+        plt.plot(pulse_profiles[i]/mean_profile, c='b', lw=0.5, linestyle='--', label='Pulse Profile / Mean Profile')
+        plt.xlabel('Phase Bin')
+        plt.ylabel('Flux Density (Jy)')
+        for k in range(len(gmps)):
+            plt.axvline(x=gmps_i[k], c=colours[k], lw=0.5, linestyle='--', label='%s<E_P>'%gmps[k])
+        plt.legend()
+        plt.savefig('GMP_'+files[i].split(os.extsep, 1)[0]+'.pdf', dpi=300)
+        plt.close()
 fig = plt.figure(figsize=(10,5), dpi=300)
 plt.plot(mean_profile)
-plt.savefig('mean_profile.pdf', dpi=300)
+plt.savefig('mean_profile_%s.pdf'%PCODE, dpi=300)
